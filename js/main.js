6106c9aa818e13881279e66fd3c4c0cbd8a160bc
Abdul Hafiz Bahrain <abdulhafizbahrain@gmail.com>
2022-08-29 17:58:56 +0800

    perubahan logikan dan sedikit design

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var valueTypeFour = [];
 var valueTypeFive = [];
 var valueTypeSix = [];
 
-const resistorBandColor = document.querySelector('#resistorBandColor');
+const resistorBandColor = document.getElementById('resistorBandColor');
 function showBandColor(showData) {
     document.getElementById('modalBandTitle').innerText = showData;
     output = '';
@@ -40,7 +40,7 @@ formula();
     
 
     for (startIndex; startIndex < data.length; startIndex++){
-        output = output + '<div class="container rounded-0 m-0 py-1 ps-1 pe-3 border-0 alert alert-' + data[startIndex].toLowerCase() + '" role="alert"> <div class="row" id="bg-' + data[startIndex].toLowerCase() + '" ><div class="col-3">' + data[startIndex] + '</div><div class="col-3 text-center">' + dataValue[startIndex] + '</div><div class="col-6 rounded-1 border-0 btn btn-' + data[startIndex].toLowerCase() + '" id="btnColor"  aria-valuenow="' + dataValue[startIndex] + '" data-bs-dismiss="modal"></div></div></div>';
+        output = output + '<div class="container rounded-0 m-0 py-1 ps-1 pe-3 border-0 alert alert-' + data[startIndex].toLowerCase() + '" role="alert"> <div class="row" id="bg-' + data[startIndex].toLowerCase() + '" ><div class="col-2">' + data[startIndex] + '</div><div class="col-4 text-end">' + convert(dataValue[startIndex]) + '</div><div class="col-6 rounded-1 border-0 btn btn-' + data[startIndex].toLowerCase() + '" id="btnColor"  aria-valuenow="' + dataValue[startIndex] + '" data-bs-dismiss="modal"></div></div></div>';
     } 
     return output;
 }
@@ -75,7 +75,7 @@ for (let index = 0; index < typeSixResistorBand.length; index++){
 // Modal choose color
 resistorBandColor.addEventListener('click', function (element) {
     if (element.target.id == 'btnColor') {
-        bandActive.classList.remove('bg-black', 'bg-brown', 'bg-red', 'bg-orange', 'bg-yellow', 'bg-green', 'bg-blue', 'bg-gray', 'bg-white', 'bg-gold', 'bg-silver', 'bg-no-color', 'bg-transparent');
+        bandActive.classList.remove('bg-black', 'bg-brown', 'bg-red', 'bg-orange', 'bg-yellow', 'bg-green', 'bg-blue', 'bg-purple', 'bg-gray', 'bg-white', 'bg-gold', 'bg-silver', 'bg-no-color', 'bg-transparent');
         bandActive.classList.add(element.target.parentElement.id);
         bandActive.ariaValueNow = element.target.ariaValueNow;
     }
@@ -87,13 +87,16 @@ formula();
 
 const navTypeResistor = document.querySelectorAll('#navTypeResistor');
 const typeResistor = document.querySelectorAll('#typeResistor > div');
+const resultValue = document.querySelectorAll('#resultValue');
 function navToggle(navIndex) {
     for (let index = 0; index < navTypeResistor.length; index++){
         navTypeResistor[index].classList.remove('active');
         typeResistor[index].classList.add('visually-hidden');
+        resultValue[index].classList.add('visually-hidden');
     }
     navTypeResistor[navIndex].classList.add('active');
     typeResistor[navIndex].classList.remove('visually-hidden');
+    resultValue[navIndex].classList.remove('visually-hidden');
 }
 
 navTypeResistor[0].addEventListener('click', function () {
@@ -117,19 +120,34 @@ navTypeResistor[3].addEventListener('click', function () {
 // Formula
 formula();
 
+function convert(dataValue) {
+    if (dataValue < 1000) {
+        return dataValue + ' Ohm';
+    }else if ((dataValue >= 1000) && (dataValue < 1000000)) {
+        return (dataValue / 1000)+ ' K Ohm';
+    }else if ((dataValue >= 1000000) && (dataValue < 1000000000)) {
+        return (dataValue / 1000000)+ ' M Ohm';
+    }else if ((dataValue >= 1000000000) && (dataValue < 1000000000000)) {
+        return (dataValue / 1000000000)+ ' G Ohm';
+    }
+}
+
 function formula() {
-    console.log(typeFourResistorBand[0].ariaValueNow);
-    console.log(typeFourResistorBand[1].ariaValueNow);
-    console.log(typeFourResistorBand[2].ariaValueNow);
-    console.log(typeFourResistorBand[3].ariaValueNow);
-    
     valueTypeFour = [typeFourResistorBand[0].ariaValueNow, typeFourResistorBand[1].ariaValueNow, typeFourResistorBand[2].ariaValueNow, typeFourResistorBand[3].ariaValueNow, 0];
     valueTypeFour[4] = (valueTypeFour[0] + valueTypeFour[1]) * valueTypeFour[2] ;
-    valueTypeFour[4] = ((valueTypeFour[4] / 100) * valueTypeFour[3] + valueTypeFour[4]) ;
-    console.log(valueTypeFour[4]);
+    parseFloat(valueTypeFour[4]);
+
+    valueTypeFive = [typeFiveResistorBand[0].ariaValueNow, typeFiveResistorBand[1].ariaValueNow, typeFiveResistorBand[2].ariaValueNow, typeFiveResistorBand[3].ariaValueNow, typeFiveResistorBand[4].ariaValueNow, 0];
+    valueTypeFive[5] = (valueTypeFive[0] + valueTypeFive[1] + valueTypeFive[2]) * valueTypeFive[3] ;
     
-    const resultTypeFourResistorBand = document.getElementById('resultTypeFourResistorBand');
-    resultTypeFourResistorBand.innerText = valueTypeFour[4];
+
+    valueTypeSix = [typeSixResistorBand[0].ariaValueNow, typeSixResistorBand[1].ariaValueNow, typeSixResistorBand[2].ariaValueNow, typeSixResistorBand[3].ariaValueNow, typeSixResistorBand[4].ariaValueNow, typeSixResistorBand[5].ariaValueNow, 0];
+    valueTypeSix[6] = (valueTypeSix[0] + valueTypeSix[1] + valueTypeSix[2]) * valueTypeSix[3] ;
+
+    resultValue[0].innerText = convert(valueTypeFour[4]);
+    resultValue[1].innerText = convert(valueTypeFive[5]);
+    resultValue[2].innerText = convert(valueTypeSix[6]);
+
     
 }
 
